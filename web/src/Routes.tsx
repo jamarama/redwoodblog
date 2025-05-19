dc6b9c3b560f33bb763c31dc8fb7e30a38220331
jamarama <153025639+jamarama@users.noreply.github.com>
2025-05-19 12:21:12 -0500

    Setting up authentication, showing login page and info

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -7,33 +7,51 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Router, Route, Set } from "@redwoodjs/router";
+import { PrivateSet, Router, Route, Set } from "@redwoodjs/router";
 
 import BlogLayout from "src/layouts/BlogLayout";
 import ScaffoldLayout from "src/layouts/ScaffoldLayout";
 
-import { useAuth } from './auth'
+import { useAuth } from "./auth";
 
 const Routes = () => {
   return (
     <Router useAuth={useAuth}>
-      <Route path="/contact" page={ContactPage} name="contact" />
-      <Set
-        wrap={ScaffoldLayout}
-        title="Posts"
-        titleTo="posts"
-        buttonLabel="New Post"
-        buttonTo="newPost"
-      >
-        <Route path="/admin/posts/new" page={PostNewPostPage} name="newPost" />
-        <Route
-          path="/admin/posts/{id:Int}/edit"
-          page={PostEditPostPage}
-          name="editPost"
-        />
-        <Route path="/admin/posts/{id:Int}" page={PostPostPage} name="post" />
-        <Route path="/admin/posts" page={PostPostsPage} name="posts" />
-      </Set>
+      <Route path="/login" page={LoginPage} name="login" />
+      <Route path="/signup" page={SignupPage} name="signup" />
+      <Route
+        path="/forgot-password"
+        page={ForgotPasswordPage}
+        name="forgotPassword"
+      />
+      <Route
+        path="/reset-password"
+        page={ResetPasswordPage}
+        name="resetPassword"
+      />
+
+      <PrivateSet unauthenticated="home">
+        <Set
+          wrap={ScaffoldLayout}
+          title="Posts"
+          titleTo="posts"
+          buttonLabel="New Post"
+          buttonTo="newPost"
+        >
+          <Route
+            path="/admin/posts/new"
+            page={PostNewPostPage}
+            name="newPost"
+          />
+          <Route
+            path="/admin/posts/{id:Int}/edit"
+            page={PostEditPostPage}
+            name="editPost"
+          />
+          <Route path="/admin/posts/{id:Int}" page={PostPostPage} name="post" />
+          <Route path="/admin/posts" page={PostPostsPage} name="posts" />
+        </Set>
+      </PrivateSet>
       <Set wrap={BlogLayout}>
         <Route path="/article/{id:Int}" page={ArticlePage} name="article" />
         <Route path="/contact" page={ContactPage} name="contact" />
